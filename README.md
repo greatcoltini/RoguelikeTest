96cfceb4dfd88ba33ce25b0bde2d72f6e5cdfab0
Colton Donkersgoed <coltonmdonk@gmail.com>
2024-02-05 19:50:05 -0500

    more progress on roguelike

---
+++ README.md
@@ -0,0 +1,3 @@
+## February 05 2024:
+- fixed blurry pixel art via project settings -> render to nearest
+- setup of character and animations - character script
---
+++ scenes/player.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+# export variables
+@export var move_speed : float = 100
+@export var starting_position : Vector2 = Vector2(0, 1)
+
+@onready var anim_tree = $AnimationTree
+@onready var state_machine = anim_tree.get("parameters/playback")
+
+@export var sprite = Sprite2D
+@export var anim_player = AnimationPlayer
+
+# init regular variables
+var paused = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	# get input direction
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	
+	if not paused:
+		if not input_direction == Vector2.ZERO:
+			input_direction = input_direction.normalized()
+		velocity = input_direction * move_speed	
+		
+		update_animation_parameters(input_direction)
+		move_and_slide()
+		pick_new_state()
+		
+		
+		
+func update_animation_parameters(move_input : Vector2):
+	if (move_input != Vector2.ZERO):
+		anim_tree.set("parameters/Walk/blend_position", move_input)
+		anim_tree.set("parameters/Idle/blend_position", move_input)
+		
+# changes animations depending on the input given
+func pick_new_state():
+	if (velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
